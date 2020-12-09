10cd76198e28d5664d1e097bd4d890a1f82bf40e
Olaf Sulich <olafsulich@gmail.com>
2020-12-09 18:01:27 +0100

    Add new recommendation

--- data/recommendations.ts
+++ data/recommendations.ts
@@ -8,15 +8,7 @@ export const recommendations = [
       company: 'Senior Frontend Engineer w SmartRecruiters',
     },
   },
-  {
-    content:
-      'Olaf jest przede wszystkim super uprzejmym gościem :D. Często udziela się w community - pomaga innym i można z nim podyskutować. Bardzo lubię poczytać sobie również jego bloga - wszystko jest na miejscu. Artykuły są dopracowane i w pełni poprawne merytorycznie oraz prezentują zagadnienia na które nie zwraca się zawsze uwagi. Polecam śledzić twórczość Olafa, bo można tym sposobem się wiele nauczyć!',
-    author: {
-      image: '/images/rekomendacje/adam-siekierski.jpg',
-      name: 'Adam Siekierski',
-      company: 'CTO w Require Podcast',
-    },
-  },
+
   {
     content: `Mam przyjemność pracować z Olafem od roku.
 Olaf Sulich zaczął swoją współpracę w Hypnocoders w 2020 jako front-end developer.
@@ -31,21 +23,29 @@ Mogę z pewnością rekomendować Olafa jako wybitną i profesjonalną osobę.`,
   },
   {
     content:
+      'Olaf jest przede wszystkim super uprzejmym gościem :D. Często udziela się w community - pomaga innym i można z nim podyskutować. Bardzo lubię poczytać sobie również jego bloga - wszystko jest na miejscu. Artykuły są dopracowane i w pełni poprawne merytorycznie oraz prezentują zagadnienia na które nie zwraca się zawsze uwagi. Polecam śledzić twórczość Olafa, bo można tym sposobem się wiele nauczyć!',
+    author: {
+      image: '/images/rekomendacje/adam-siekierski.jpg',
+      name: 'Adam Siekierski',
+      company: 'CTO w Require Podcast',
+    },
+  },
+  {
+    content:
+      'Szczerze polecam bloga, pomógł mi zrozumieć kwestie które były dla mnie niejasne. Dodatkowo, Olaf to bardzo miły i pomocny człowiek, kopalnia wiedzy jeśli chodzi o Frontend. Ode mnie, piątka z plusem :)',
+    author: {
+      image: '/images/rekomendacje/grzegorz-cymborski.jpg',
+      name: 'Grzegorz Cymborski',
+      company: 'Frontend Developer w Hypnocoders/Freelancer',
+    },
+  },
+  {
+    content:
       "Dzięki Olafowi z  Frontlive udało mi się dostać moją pierwszą pracę w React! Jego kurs z TypeScript + React to według mnie najlepsze źródło jakie mogłem znaleźć na początku mojej drogi z TypeScriptem. Moim następnym celem jest nauka Gatsby'ego tak więc mam nadzieję, że Frontlive również tutaj mnie nie zawiedzie!",
     author: {
       image: '/images/rekomendacje/szymon-hrabia.jpg',
       name: 'Szymon Hrabia',
       company: 'Frontend Developer w Parkalot',
-    },
-  },
-
-  {
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-    author: {
-      image: '/images/user.png',
-      name: 'John Doe',
-      company: 'Vercel',
     },
   },
 ];
